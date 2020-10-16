1c39b6eb5482a0e073512161cdd28b0c6392edc1
kingelijah <ealeakhue@gmail.com>
2020-10-16 08:15:57 +0100

    Updates

---
+++ precache-manifest.16808be70af532d13f3ea0e3f0c93e9e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19572f9e1eb829022202fb2ec5a1b09",
+    "url": "/disputeresolutiongit%20init/index.html"
+  },
+  {
+    "revision": "3bf551fb6eed15777eca",
+    "url": "/disputeresolutiongit%20init/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "de7db3e6176976fad303",
+    "url": "/disputeresolutiongit%20init/static/css/main.7494ea68.chunk.css"
+  },
+  {
+    "revision": "3bf551fb6eed15777eca",
+    "url": "/disputeresolutiongit%20init/static/js/2.af8124ed.chunk.js"
+  },
+  {
+    "revision": "254c01d1a3c7d8124adf4c35e46b76e3",
+    "url": "/disputeresolutiongit%20init/static/js/2.af8124ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de7db3e6176976fad303",
+    "url": "/disputeresolutiongit%20init/static/js/main.c8310086.chunk.js"
+  },
+  {
+    "revision": "22a5079bf8c3ac15653f",
+    "url": "/disputeresolutiongit%20init/static/js/runtime-main.041e9c1d.js"
+  },
+  {
+    "revision": "feec6a8d802825b279b0b03b0fa6f141",
+    "url": "/disputeresolutiongit%20init/static/media/edit.feec6a8d.svg"
+  },
+  {
+    "revision": "f407e100e61394e897cf0bdaf8d25cc2",
+    "url": "/disputeresolutiongit%20init/static/media/user.f407e100.svg"
+  }
+]);
